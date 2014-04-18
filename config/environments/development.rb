4ce6e8da19e552b857bef4f9b0911fbff658b3b2
Anne Jhetson <student@epicodus.com>
2014-04-18 09:12:00 -0700

    setup devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ PainterGuy::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #Define default URL
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
