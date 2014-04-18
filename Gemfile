afd7ab5216225c8b342c1c4820413d620d77c6aa
Anne Jhetson <student@epicodus.com>
2014-04-18 09:05:26 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'devise'
+gem 'bootstrap-sass'
+
+
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'launchy'
+  gem 'pry'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PainterGuy::Application.config.secret_key_base = '60d4d092e258ac0892d97cc0ffd1ecde214f1abd695eac646dc3640160de5c318ea4af0eb4da03d4b9988425eede991317b6a60ec683e17148c6e75b3aa6bb23'
